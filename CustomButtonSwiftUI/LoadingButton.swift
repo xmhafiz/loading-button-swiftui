0b9cb3306370bb21528618d9bd1254734bb60de0
Mohd Hafiz <xmhafiz@gmail.com>
2023-08-05 09:23:23 +0800

    added loading button

--- CustomButtonSwiftUI/LoadingButton.swift
+++ CustomButtonSwiftUI/LoadingButton.swift
@@ -7,6 +7,34 @@
 
 import SwiftUI
 
-struct LoadingButton {
+struct LoadingButton: View {
+    var title: String
+    @Binding var isLoading: Bool
+    var loadingText: String?
+    var backgroundColor: Color?
+    var loaderTintColor: Color?
+    var action: (() -> Void)?
     
+    var body: some View {
+        Button(action: {
+            action?()
+        }) {
+            HStack (spacing: 16) {
+                if isLoading {
+                    if let loadingText {
+                        Text(loadingText).foregroundColor(.white)
+                    }
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .tint(loaderTintColor ?? .white)
+                    
+                } else {
+                    Text(title).foregroundColor(.white)
+                }
+            }.frame(maxWidth: .infinity)
+        }
+        .frame(height: 54)
+        .background(backgroundColor ?? .blue)
+        .cornerRadius(8)
+    }
 }
